dcf6aefc78e6fd61ac851807fcd6f08bf37a2908
Guru <yusuphjoluwaseun@gmail.com>
2023-05-10 03:52:34 +0100

    Initial Commit

---
+++ UniUnderground/UniUndergroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniUndergroundApp.swift
+//  UniUnderground
+//
+//  Created by Guru King on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct UniUndergroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
